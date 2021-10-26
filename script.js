bcb87328a65dabb18d91bea5446f028f0fa5acf0
User <Ciloci Pavel>
2021-10-27 00:35:27 +0300

    Am transformat numerele din string in number , a ramas sa adaugati functiile cu impartire , si sa scoateme rezultatele pe html

--- script.js
+++ script.js
@@ -5,23 +5,27 @@ const buttons = document.querySelectorAll("button")
 buttons.forEach(item => {
     item.addEventListener('click' ,(e) => {
         let result =document.querySelector('.rezultat').value;
+
         const one = document.querySelector(".input1").value
         const two = document.querySelector(".input2").value
 
         if (e.target.innerHTML === "+" ) {
-            result = plus(one,two)
+            result = plus(parseInt(one),parseInt(two))
             console.log(result)
+
+
         }
         else if (e.target.innerHTML==="-"){
-            result=minus(one,two)
+            result=minus(parseInt(one),parseInt(two))
             console.log(result)
+
         }
         else if (e.target.innerHTML==="/"){
-            result=devide(one,two)
+            result=devide(parseInt(one),parseInt(two))
             console.log(result)
         }
         else if (e.target.innerHTML==="*"){
-            result=multiply(one,two)
+            result=multiply(parseInt(one),parseInt(two))
             console.log(result)
         }
 
@@ -30,6 +34,6 @@ buttons.forEach(item => {
 
 const plus = (num1, num2) =>  num1 + num2
 
-
 const minus = (num1, num2) =>num1 - num2
+
 
