91fe9b41aadd93dd38fe647cb47963a2cdf71592
AlexandruGru <agrumeza1@gmail.com>
2021-10-28 00:59:05 +0300

    refactored index.html, only usefult elements remains; added style.css; js file refactored and now it works

--- script.js
+++ script.js
@@ -1,48 +1,37 @@
-const multiply = (num1,num2) =>{
-    let resultNum = num1*num2
-}
+const buttons = document.querySelectorAll("button");
 
-
-const buttons = document.querySelectorAll("button")
+let resultOutput = document.querySelector('.result');
 
 buttons.forEach(item => {
     item.addEventListener('click' ,(e) => {
-        let result =document.querySelector('.result').value;
 
-        const one = document.querySelector(".input1").value
-        const two = document.querySelector(".input2").value
+        const one = +document.querySelector(".input1").value;
+        const two = +document.querySelector(".input2").value;
 
         if (e.target.innerHTML === "+" ) {
-            result = plus(parseInt(one),parseInt(two))
-            console.log(result)
-
-
+            resultOutput.textContent = `reusltatul adunarii este: ${plus(one, two)}`;
         }
-        else if (e.target.innerHTML==="-"){
-            result=minus(parseInt(one),parseInt(two))
-            console.log(result)
 
+        else if (e.target.innerHTML==="-"){
+            resultOutput.textContent = `reusltatul scaderii este: ${minus(one, two)}`;
         }
         else if (e.target.innerHTML==="/"){
-            result=devide(parseInt(one),parseInt(two));
-            // console.log(result)
+            resultOutput.textContent = `reusltatul inpartirii este: ${devide(one, two)}`;
         }
         else if (e.target.innerHTML==="*"){
-            result=multiply(parseInt(one),parseInt(two))
-            console.log(result)
+            resultOutput.textContent = `reusltatul inmultirii este: ${multiply(one, two)}`;
         }
 
     })});
 
 
-const plus = (num1, num2) =>  num1 + num2
+const plus = (num1, num2) =>  num1 + num2;
 
-const minus = (num1, num2) =>num1 - num2
+const minus = (num1, num2) => num1 - num2;
 
-const devide = (num1, num2) => {
-    let resultDevide = num1/num2;
-    console.log(resultDevide);
-}
+const devide = (num1, num2) => num1 / num2;
+
+const multiply = (num1,num2) => num1 * num2;
 
 
 
